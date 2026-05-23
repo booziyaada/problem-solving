e7574b899a2c042232b8b55925c299e2065ca3c1
MurshidUsman-commits <murshidusman3@example.com>
2026-05-23 20:51:30 +0530

    WIP

---
+++ first_occurance_str.js
@@ -0,0 +1,9 @@
+var strStr = function(haystack, needle) {
+
+    for (let i = 0; i <= haystack.length; i++) {
+        if (haystack.startsWith(needle, i)) {
+            return i;
+        }
+    }
+    return -1;
+};
