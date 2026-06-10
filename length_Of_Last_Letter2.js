9ba6b0eb23639cdd6d895165958d4c7d1e17447e
MurshidUsman-commits <murshidusman3@example.com>
2026-06-10 18:44:22 +0530

    length_Of_Last_Letter2.js add other solution

---
+++ length_Of_Last_Letter2.js
@@ -0,0 +1,18 @@
+var lengthOfLastWord = function (s) {
+  let p = "";
+  let j = s.length - 1;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === " ") continue;
+    else {
+      j = i;
+      break;
+    }
+  }
+
+  for (j; j >= 0; j--) {
+    if (s[j] !== " ") p = p + s[j];
+    else break;
+  }
+
+  return p.length;
+};
